7cb7f4c9f0108e932d220c54f7d9c4cfcc22e566
pragatisinghc33-afk <pragatisinghc33@gmail.com>
2026-07-14 22:01:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function(){
+
+        const value = this.innerText;
+
+        if(value === "C"){
+
+            display.value = "";
+
+        }
+
+        else if(value === "⌫"){
+
+            display.value = display.value.slice(0,-1);
+
+        }
+
+        else if(value === "="){
+
+            try{
+
+                display.value = eval(display.value);
+
+            }
+
+            catch{
+
+                display.value = "Error";
+
+            }
+
+        }
+
+        else{
+
+            display.value += value;
+
+        }
+
+    });
+
+});
